b3f846e121795baa6fb55d71140a1d403e34cd77
leon.dahmer <leon.dahmer@gmail.com>
2019-01-05 21:49:14 -0500

    Stripe Test

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -32,32 +32,26 @@ class Devise::RegistrationsController < DeviseController
       :customer => customer.id
     )
     
-    if charge.paid && charge.amount == 5000
+    resource.stripe_token = customer.id
     
-      resource.stripe_token = customer.id
-    
-      resource.save
-      yield resource if block_given?
-      if resource.persisted?
-        if resource.active_for_authentication?
-          set_flash_message! :notice, :signed_up
-          sign_up(resource_name, resource)
-          respond_with resource, location: after_sign_up_path_for(resource)
-        else
-          set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
-          expire_data_after_sign_in!
-          respond_with resource, location: after_inactive_sign_up_path_for(resource)
-        end
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
       else
-        clean_up_passwords resource
-        set_minimum_password_length
-        respond_with resource
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
       end
-    else 
-      flash[:error] = "Zahlung war nicht erfolgreich"
-      redirect_to new_user_registration_path
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
     end
-      
+    
     rescue Stripe::CardError => e
       flash[:error] = e.message
       redirect_to new_user_registration_path
