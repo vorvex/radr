bbcdcf0cd4d37d42fcbe7826840ffd071930dfb7
leon.dahmer <leon.dahmer@gmail.com>
2019-02-10 10:28:39 -0500

    API changes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,10 +12,20 @@ class Api::UsersController < ApplicationController
   
   def refresh_event_status
     @user.events.each do |event|
-      if event.end_time < Time.now && event.status != "beendet"
-        event.status = "beendet"
-        event.save!
-        UserMailer.with(user: @user, event: event).event_beendet.deliver_now
+      if event.status != "beendet" 
+        if event.end_time.nil?
+          if event.start_time < Time.now 
+            event.status = "beendet"
+            event.save!
+            UserMailer.with(user: @user, event: event).event_beendet.deliver_now
+          end
+        else 
+          if event.end_time < Time.now 
+            event.status = "beendet"
+            event.save!
+            UserMailer.with(user: @user, event: event).event_beendet.deliver_now
+          end
+        end
       end
     end
   end
